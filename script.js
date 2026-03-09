b97963dbdc670bf868a0d66e9c1c66d9d94261b9
PedroThink <97687650+PedroThink@users.noreply.github.com>
2026-03-09 15:01:29 +0000

    Adicionado gerador de escolha do computador e prompt para escolha do usuário

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+// Escolha do computador
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3); //Declara a variável randomNumber e atribui-lhe um número aleatório, multiplicado por 3 e arredondado para baixo. Opções: 0, 1, 2;
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+};
+    
+
+console.log(getComputerChoice());
+
+//Escolha do jogador
+function getHumanChoice() {
+    const choice = prompt("Escolhe: rock, paper ou scissors");
+    return choice;
+};
+
+console.log(getHumanChoice());
