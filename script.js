9ec0ae03230c28d7f90f5ef736a2963f69cbeb59
PedroThink <97687650+PedroThink@users.noreply.github.com>
2026-03-14 10:56:06 +0000

    Atualizado para uma rodada de jogo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // Escolha do computador
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3); //Declara a variável randomNumber e atribui-lhe um número aleatório, multiplicado por 3 e arredondado para baixo. Opções: 0, 1, 2;
@@ -20,4 +23,32 @@ function getHumanChoice() {
     return choice;
 };
 
-console.log(getHumanChoice());
+// Jogo de uma rodada
+function playRound(humanChoice, computerChoice) {
+    // tornar humanChoise insensível a case
+    humanChoice = humanChoice.toLowerCase();
+
+    const wins = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
+
+    if (humanChoice === computerChoice) {
+        console.log("Empate!");
+        return;
+    }
+
+    if (wins[humanChoice] === computerChoice) {
+        humanScore++;
+        console.log("O humano ganhou a rodada!");
+    } else {
+        computerScore++;
+        console.log("O computador ganhou a rodada!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
