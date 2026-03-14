19a6de081420259e5b1cd44f5226017fb889b36b
PedroThink <97687650+PedroThink@users.noreply.github.com>
2026-03-14 21:14:35 +0000

    Projeto terminado

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 // Escolha do computador
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3); //Declara a variável randomNumber e atribui-lhe um número aleatório, multiplicado por 3 e arredondado para baixo. Opções: 0, 1, 2;
@@ -13,9 +10,6 @@ function getComputerChoice() {
         return "scissors";
     }
 };
-    
-
-console.log(getComputerChoice());
 
 //Escolha do jogador
 function getHumanChoice() {
@@ -24,7 +18,13 @@ function getHumanChoice() {
 };
 
 // Jogo de uma rodada
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice, scores) {
+    
+    if (!humanChoice) {
+        console.log("Escolha Inválida.");
+        return;
+    }
+    
     // tornar humanChoise insensível a case
     humanChoice = humanChoice.toLowerCase();
 
@@ -40,15 +40,39 @@ function playRound(humanChoice, computerChoice) {
     }
 
     if (wins[humanChoice] === computerChoice) {
-        humanScore++;
+        scores.humanScore++;
         console.log("O humano ganhou a rodada!");
     } else {
-        computerScore++;
+        scores.computerScore++;
         console.log("O computador ganhou a rodada!");
     }
-}
+};
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// Jogo completo
+function playGame () {
 
-playRound(humanSelection, computerSelection);
+    const scores = {
+        humanScore: 0,
+        computerScore: 0
+    };
+
+    for (let i = 0; i < 5; i++) {
+
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection, scores);
+
+        console.log(`Pontuação: Humano ${scores.humanScore} - Computador ${scores.computerScore}`);
+    }
+
+    if (scores.humanScore > scores.computerScore) {
+        console.log("🎉 O humano venceu o jogo!");
+    } else if (scores.computerScore > scores.humanScore) {
+        console.log("💻 O computador venceu o jogo!");
+    } else {
+        console.log("🤝 O jogo terminou empatado!");
+    }
+};
+
+playGame();
